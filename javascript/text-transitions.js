b403191eb8c7c8716630003f657b27ccf7932c42
Niki <ntubacki@mit.edu>
2016-06-15 21:56:56 -0700

    initial commit

---
+++ javascript/text-transitions.js
@@ -0,0 +1,79 @@
+$(document).ready(function() {
+  $('#resume').mouseover(function() {
+    // $('#resume-text').effect("shake", {direction: "up"});
+    $('#resume-text').css("visibility", "visible");
+    // $('#resume-text').fadeIn();
+
+    // $('#resume-text').toggle("pulsate");
+  });
+});
+
+$(document).ready(function() {
+  $('#resume').mouseout(function() {
+    $('#resume-text').css("visibility", "hidden");
+  });
+});
+
+$(document).ready(function() {
+  $('#linkedin').mouseover(function() {
+    // $('#resume-text').effect("shake", {direction: "up"});
+    $('#linkedin-text').css("visibility", "visible");
+    // $('#resume-text').fadeIn();
+
+    // $('#resume-text').toggle("pulsate");
+  });
+});
+
+$(document).ready(function() {
+  $('#linkedin').mouseout(function() {
+    $('#linkedin-text').css("visibility", "hidden");
+  });
+});
+
+$(document).ready(function() {
+  $('#github').mouseover(function() {
+    // $('#resume-text').effect("shake", {direction: "up"});
+    $('#github-text').css("visibility", "visible");
+    // $('#resume-text').fadeIn();
+
+    // $('#resume-text').toggle("pulsate");
+  });
+});
+
+$(document).ready(function() {
+  $('#github').mouseout(function() {
+    $('#github-text').css("visibility", "hidden");
+  });
+});
+
+$(document).ready(function() {
+  $('#codepen').mouseover(function() {
+    // $('#resume-text').effect("shake", {direction: "up"});
+    $('#codepen-text').css("visibility", "visible");
+    // $('#resume-text').fadeIn();
+
+    // $('#resume-text').toggle("pulsate");
+  });
+});
+
+$(document).ready(function() {
+  $('#codepen').mouseout(function() {
+    $('#codepen-text').css("visibility", "hidden");
+  });
+});
+
+$(document).ready(function() {
+  $('#contact').mouseover(function() {
+    // $('#resume-text').effect("shake", {direction: "up"});
+    $('#contact-text').css("visibility", "visible");
+    // $('#resume-text').fadeIn();
+
+    // $('#resume-text').toggle("pulsate");
+  });
+});
+
+$(document).ready(function() {
+  $('#contact').mouseout(function() {
+    $('#contact-text').css("visibility", "hidden");
+  });
+});

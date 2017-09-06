6843f58434c6614855137f437bf7b8482769f5be
Niki <ntubacki@mit.edu>
2017-09-06 16:12:33 -0400

    website update to semantic

--- javascript/text-transitions.js
+++ javascript/text-transitions.js
@@ -1,3 +1,13 @@
+// import mojs from 'mojs';
+// var mojs = require("mo-js");
+
+$(document).ready(function() {
+  $('#project1').mouseover(function() {
+    $('#project1info').css("visibility", "visible");
+    $('#aboutme').css("visibility", "hidden");
+  });
+});
+
 $(document).ready(function() {
   $('#resume').mouseover(function() {
     // $('#resume-text').effect("shake", {direction: "up"});
@@ -16,11 +26,10 @@ $(document).ready(function() {
 
 $(document).ready(function() {
   $('#linkedin').mouseover(function() {
-    // $('#resume-text').effect("shake", {direction: "up"});
+    // $('#linkedin-text').effect("shake", {direction: "up"});
     $('#linkedin-text').css("visibility", "visible");
-    // $('#resume-text').fadeIn();
 
-    // $('#resume-text').toggle("pulsate");
+    // $('#linkedin-text').toggle("pulsate");
   });
 });
 
@@ -63,9 +72,9 @@ $(document).ready(function() {
 });
 
 $(document).ready(function() {
-  $('#contact').mouseover(function() {
+  $('#email').mouseover(function() {
     // $('#resume-text').effect("shake", {direction: "up"});
-    $('#contact-text').css("visibility", "visible");
+    $('#email-text').css("visibility", "visible");
     // $('#resume-text').fadeIn();
 
     // $('#resume-text').toggle("pulsate");
@@ -73,7 +82,64 @@ $(document).ready(function() {
 });
 
 $(document).ready(function() {
-  $('#contact').mouseout(function() {
-    $('#contact-text').css("visibility", "hidden");
+  $('#email').mouseout(function() {
+    $('#email-text').css("visibility", "hidden");
   });
 });
+
+// const fireColors = ["#E25822", "#E27822", "#E29822", "#E2B822", "#E23822", "#E2222C", "#E2224C"]
+
+// const SWIRL_OPTS = {
+//   left: 0, top: 0,
+//   duration:       'rand(1000, 5000)',
+//   radius:         'rand(1, 5)',
+//   pathScale:      'rand(.5, 1)',
+//   swirlFrequency: 'rand(2,10)',
+//   swirlSize: 'rand(2,20)',
+// }
+
+// const swirlsNumber = 400;
+// let swirlsArray = [];
+
+// function createParticles() {
+//   for(var i=0;i<swirlsNumber;i++){
+//     let swirlColor = fireColors[Math.floor(Math.random() * fireColors.length)];
+
+
+//     var swirl = new mojs.ShapeSwirl({
+//       ...SWIRL_OPTS,
+//       fill: swirlColor,
+//     });
+//     swirlsArray.push(swirl);  
+//   } 
+// }
+
+// function drawParticles() {
+//   const y = { [window.innerHeight]: 0 };
+  
+//   for(var i=0;i<swirlsNumber;i++){
+    
+//     var x = i * window.innerWidth/swirlsNumber;
+    
+//     swirlsArray[i] 
+//       .tune({ x, y })
+//       .generate()
+//       .replay();
+//   }
+// }
+
+
+// document.addEventListener('click', function(){
+  
+//   drawParticles();
+  
+// });
+
+// createParticles();
+// drawParticles();
+
+// $(document).ready(function() {
+//   alert("YO");
+//   createParticles();
+//   drawParticles();
+// });
